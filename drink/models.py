3d510d4ec06b3a13de6f8b803f6e2992c273281d
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-01-31 21:08:47 +0100

    Update models.py

--- drink/models.py
+++ drink/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Drink(models.Model):
+    """
+    name: Drink
+    description: This class keep record on drink
+    """
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     
